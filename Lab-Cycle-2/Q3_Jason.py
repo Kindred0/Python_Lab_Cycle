25ed680f5cbb0b9320a3a9c7f589094a32d61bf8
Lenat Thomas <95519891+Kindred0@users.noreply.github.com>
2022-04-04 11:19:49 +0530

    Add files via upload

--- Lab-Cycle-2/Q3_Jason.py
+++ Lab-Cycle-2/Q3_Jason.py
@@ -1,6 +1,38 @@
 import json
 
 with open('iris.json') as f:
-    data=json.load(f)
+	data=json.load(f)
 
-print(data)
+p_area=1000
+s_area=0
+for i in data:
+	if i['species']=='setosa':
+		print(i)
+		if (i['sepalLength']*i['sepalWidth'])>s_area:
+			s_area=i['sepalLength']*i['sepalWidth']
+		if p_area>(i['petalLength']*i['petalWidth']):
+			p_area=i['petalLength']*i['petalWidth']
+print("Minimum petal area of the species 'Setosa'\t: ",p_area)
+print("Maximum sepal area of the species 'Setosa'\t: ",s_area)
+p_area=1000
+s_area=0
+for i in data:
+	if i['species']=='versicolor':
+		if (i['sepalLength']*i['sepalWidth'])>s_area:
+			s_area=i['sepalLength']*i['sepalWidth']
+		if p_area>(i['petalLength']*i['petalWidth']):
+			p_area=i['petalLength']*i['petalWidth']
+print("Minimum petal area of the species 'Versicolor'\t: ",p_area)
+print("Maximum sepal area of the species 'Versicolor'\t: ",s_area)
+p_area=1000
+s_area=0
+for i in data:
+	if i['species']=='virginica':
+		if (i['sepalLength']*i['sepalWidth'])>s_area:
+			s_area=i['sepalLength']*i['sepalWidth']
+		if p_area>(i['petalLength']*i['petalWidth']):
+			p_area=i['petalLength']*i['petalWidth']
+print("Minimum petal area of the species 'Virginica'\t: ",p_area)
+print("Maximum sepal area of the species 'Virginica'\t: ",s_area)
+
+
