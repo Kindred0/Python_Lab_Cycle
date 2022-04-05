33bb5b3d643662c5e974d8a0f136e76c60704075
Lenat Thomas <95519891+Kindred0@users.noreply.github.com>
2022-04-05 15:07:19 +0530

    Add files via upload

--- Lab-Cycle-2/Q3_Jason.py
+++ Lab-Cycle-2/Q3_Jason.py
@@ -6,33 +6,48 @@ with open('iris.json') as f:
 p_area=1000
 s_area=0
 for i in data:
+	petalarea=i['petalLength']*i['petalWidth']
+	sepalarea=i['sepalLength']*i['sepalWidth']
 	if i['species']=='setosa':
 		print(i)
-		if (i['sepalLength']*i['sepalWidth'])>s_area:
-			s_area=i['sepalLength']*i['sepalWidth']
-		if p_area>(i['petalLength']*i['petalWidth']):
-			p_area=i['petalLength']*i['petalWidth']
-print("Minimum petal area of the species 'Setosa'\t: ",p_area)
+		if (sepalarea)>s_area:
+			s_area=sepalarea
+		if p_area>petalarea:
+			p_area=petalarea
+print("\nMinimum petal area of the species 'Setosa'\t: ",p_area)
 print("Maximum sepal area of the species 'Setosa'\t: ",s_area)
 p_area=1000
 s_area=0
 for i in data:
+	petalarea=i['petalLength']*i['petalWidth']
+	sepalarea=i['sepalLength']*i['sepalWidth']
 	if i['species']=='versicolor':
-		if (i['sepalLength']*i['sepalWidth'])>s_area:
-			s_area=i['sepalLength']*i['sepalWidth']
-		if p_area>(i['petalLength']*i['petalWidth']):
-			p_area=i['petalLength']*i['petalWidth']
+		if (sepalarea)>s_area:
+			s_area=sepalarea
+		if p_area>(petalarea):
+			p_area=petalarea
 print("Minimum petal area of the species 'Versicolor'\t: ",p_area)
 print("Maximum sepal area of the species 'Versicolor'\t: ",s_area)
 p_area=1000
 s_area=0
 for i in data:
+	petalarea=i['petalLength']*i['petalWidth']
+	sepalarea=i['sepalLength']*i['sepalWidth']
 	if i['species']=='virginica':
-		if (i['sepalLength']*i['sepalWidth'])>s_area:
-			s_area=i['sepalLength']*i['sepalWidth']
-		if p_area>(i['petalLength']*i['petalWidth']):
-			p_area=i['petalLength']*i['petalWidth']
+		if (sepalarea)>s_area:
+			s_area=sepalarea
+		if p_area>(petalarea):
+			p_area=petalarea
 print("Minimum petal area of the species 'Virginica'\t: ",p_area)
 print("Maximum sepal area of the species 'Virginica'\t: ",s_area)
 
+for i in data:
+	petalarea=i['petalLength']*i['petalWidth']
+	sepalarea=i['sepalLength']*i['sepalWidth']
+	totalarea=sepalarea+petalarea
+	i.update({'totalarea':totalarea})
 
+sort_data=sorted(data, key = lambda i:i['totalarea'])
+print("\nSorted dictionary\t: \n")
+for i in sort_data:
+	print(i)
