bdeff6645367e7e0662bc5e025a995ae0938e5f7
Lenat Thomas <95519891+Kindred0@users.noreply.github.com>
2022-05-10 14:05:56 +0530

    Add files via upload

--- mine.py
+++ mine.py
@@ -1,6 +1,6 @@
-import pygame
+
 import random
-from pygame.locals import *
+
 
 
 state = []
@@ -11,7 +11,7 @@ for i in range(0,15) :
 
 mines = 25
 
-Fail = false
+Fail = False
 
 while mines != 0 :
 	x = random.randint(0,14)
@@ -19,25 +19,26 @@ while mines != 0 :
 	if state[x][y] != -1 :
 		state[x][y] = -1
 		mines = mines - 1
-		if x == 1 :
+		if x == 0 :
 			for i in range(y,y+2) :
-				for k in range(x,x+3) :
-					if state[x][y] != -1 :
-						state[x][y] = state[x][y] + 1
-		elif y == 1 :
-			for i in range(y,y+3) :
-                                for k in range(x,x+2) :
-                                        if state[x][y] != -1 :
-                                                state[x][y] = state[x][y] + 1
-		elif x == 1 and y == 1 :
+				for k in range(x-1,x+2) :
+					if state[i][k] != -1 :
+						state[i][k] = state[i][k] + 1
+		elif y == 0 :
+			for i in range(y-1,y+2) :
+                for k in range(x,x+2) :
+                    if state[i][k] != -1 :
+                        state[i][k] = state[i][k] + 1
+		
+		elif x == 0 and y == 0 :
 			for i in range(y,y+2) :
-                                for k in range(x,x+2) :
-                                        if state[x][y] != -1 :
-                                                state[x][y] = state[x][y] + 1
+                for k in range(x,x+2) :
+                    if state[i][k] != -1 :
+                        state[i][k] = state[i][k] + 1
 		else :
 			for i in range(y,y+3) :
-                                for k in range(x,x+3) :
-                                        if state[x][y] != -1 :
-                                                state[x][y] = state[x][y] + 1
+                for k in range(x,x+3) :
+                    if state[i][k] != -1 :
+                        state[i][k] = state[i][k] + 1
 
 print(state)
